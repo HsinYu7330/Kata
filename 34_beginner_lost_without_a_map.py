64c3dea96fed5b5b897d0d719f2c52e641149532
unknown <d0107330@gmail.com>
2019-12-10 16:22:13 +0800

    34. Beginner-Lost Without a Map

---
+++ 34_beginner_lost_without_a_map.py
@@ -0,0 +1,12 @@
+'''
+Beginner-Lost Without a Map
+
+Given an array of integers, return a new array with each value doubled
+ex.
+[1, 2, 3] --> [2, 4, 6] 
+'''
+
+def maps(a):
+	return [x*2 for x in a]
+
+
